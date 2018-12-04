e80730146f1bc88f2ba48a9150900c391c1639f6
catchrishi19 <43848828+catchrishi19@users.noreply.github.com>
2018-12-04 14:36:52 +0530

    Update GameController.java

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -28,8 +28,8 @@ public class GameController {
     @RequestMapping("/new")
     public ModelAndView newGame() {
         ModelAndView mav = new ModelAndView("game/edit");
-        Universe universe = new Universe();
-        mav.addObject("universe", universe);
+        //Universe universe = new Universe();
+        //mav.addObject("universe", universe);
         thinkABit(250); // Pause for random time
         return mav;
     }
